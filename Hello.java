430e2debe2fb52adb64c0b6d2c9fbd92ad3dde6d
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-23 15:56:52 +0300

    variables

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 class Hello {
     public static void main(String a[]) {
+        // Hello World
         System.out.println(6);
         System.out.println("Hello World!");
+        // Variables
+        int num1 = 4;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
