ce0fb92a447dd85dc4e00c6556caa624e46c4d73
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-26 20:16:37 +0300

    while loop

---
+++ Loops.java
@@ -0,0 +1,10 @@
+class Loops{
+    public static void main(String a[]){
+        // while loop
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
