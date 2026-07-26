d4090176acfaddd0688417a575a18c67456947e4
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-26 14:35:24 +0300

    if else condition

---
+++ Conditions.java
@@ -0,0 +1,16 @@
+public class Conditions {
+    public static void main(String a[]){
+        // if else
+        int x = 5;
+        int y = 7;
+        if(x>y)
+        {
+            System.out.println("x is greater than y");
+        }
+        else
+        {
+            System.out.println("y is greater than x");
+        }
+        
+    }
+}
