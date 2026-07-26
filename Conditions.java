2a5642cbc8c1c5a929f20f9f4aca655d540ee02a
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-26 18:55:15 +0300

    switch statements

--- Conditions.java
+++ Conditions.java
@@ -26,5 +26,34 @@ public class Conditions {
         boolean isEven; 
         isEven = x%2==0 ? true : false;
         System.out.println(isEven);
+
+        // switch
+        int n = 1;
+        switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3: 
+                System.out.println("Wednesday");
+                break;
+            case 4: 
+                System.out.println("Thursday");
+                break;
+            case 5: 
+                System.out.println("Friday");
+                break;
+            case 6: 
+                System.out.println("Saturday");
+                break;
+            case 7: 
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
     }
 }
