431fe75cfbb9d40ae2a73d01a96916eb68c9c865
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-31 20:02:44 +0300

    array

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String a[]){
+        int arr[] = new int[4];
+        arr[0] = 2;
+        arr[1] = 7;
+        arr[2] = 0;
+        arr[3] = 1;
+        System.out.println(arr[2]);
+
+        int var[] = {1, 8, 6, 2};
+        System.out.println(var[3]);
+        for(int i=0; i<4; i++){
+            System.out.println(arr[i]);
+        }   
+    }
+}
