40732a1d16a484d3efbc4b5b5b9eb667622bc1f9
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-07-31 23:15:19 +0300

    added jagged loop

--- Jagged.java
+++ Jagged.java
@@ -1,10 +1,20 @@
 public class Jagged {
     public static void main(String a[]) {
+        // Jagged
         int arr[][] = new int[3][];
         arr[0] = new int[2];
         arr[1] = new int[5];
         arr[2] = new int[3];
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = (int)(Math.random() * 10);
+                    System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // 3D array
         int threed[][][] = new int[2][3][1];
 
         for (int i = 0; i < 2; i++) {
