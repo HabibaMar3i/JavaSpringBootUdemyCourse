258d8b5059aa878c747081e1c6fe048bdd977a2f
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-08-04 20:19:46 +0300

    added constructor and parametrized constructor

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,6 +2,17 @@ class Human{
     private String name;
     private int age;
 
+    // Constructor
+    public Human(){
+        System.out.println("Test instructor");
+    }
+
+    // Parametrized Constructor
+    public Human(String n, int a){
+        name = n;
+        age = a;
+    }
+
     public String getName(){
         return name;
     }
@@ -24,6 +35,8 @@ public class Encapsulation {
         Human person = new Human();
         person.setName("Habiba");
         person.setAge(24);
+        Human person2 = new Human("Biba", 26);
         System.out.println("Name: " + person.getName() + " and Age: " + person.getAge());
+        System.out.println("Name: " + person2.getName() + " and Age: " + person2.getAge());
     }
 }
