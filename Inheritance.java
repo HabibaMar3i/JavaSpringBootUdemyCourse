838ccea0432fe9f9ce164aa947b3cecab93c0dcb
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-08-06 20:59:01 +0300

    inheritance

---
+++ Inheritance.java
@@ -0,0 +1,9 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        AdvancedCalc calc = new AdvancedCalc();
+        System.out.println(calc.add(4, 5));
+        System.out.println(calc.sub(9, 1));
+        System.out.println(calc.multi(3, 2));
+        System.out.println(calc.div(12, 3));
+    }
+}
