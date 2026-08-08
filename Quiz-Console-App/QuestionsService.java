3d5036d36893f05bdb26d45b2fb66f996cd242f0
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-08-08 15:42:52 +0300

    project 1- play quiz

--- Quiz-Console-App/QuestionsService.java
+++ Quiz-Console-App/QuestionsService.java
@@ -1,5 +1,10 @@
+
+import java.util.Scanner;
+
 public class QuestionsService {
+
     Question[] questions = new Question[5];
+    String selection[] = new String[5];
 
     public QuestionsService() {
         questions[0] = new Question(1, "What is the size of char in Java?", "1 byte", "2 bytes", "4 bytes", "8 bytes", "2 bytes");
@@ -9,9 +14,22 @@ public class QuestionsService {
         questions[4] = new Question(5, "What is the size of long in Java?", "4 bytes", "8 bytes", "16 bytes", "32 bytes", "8 bytes");
     }
 
-    public void displayQuestions(){
-        for(Question q: questions){
+    public void playQuiz() {
+        int i = 0;
+        for (Question q : questions) {
             System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner sc = new Scanner(System.in);
+            selection[i] = sc.nextLine();
+            i++;
+        }
+
+        System.out.println("Answers: ");
+        for (String s : selection) {
+            System.out.println(s);
         }
     }
 }
